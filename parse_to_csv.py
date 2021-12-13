88ebe9016cdd6c5d66f8377c6aee5b7b6f7a2c6a
Nicolas De Smyter <nicolasdesmyter@gmail.com>
2021-12-13 18:56:10 +0100

    Updated for 2022

--- parse_to_csv.py
+++ parse_to_csv.py
@@ -1,14 +1,14 @@
 import json
 
-f = open("calendar2021.csv", "w")
+f = open("calendar2022.csv", "w")
 
 f.write("Subject,Start Date,End Date,All Day Event,Description\n")
-with open('json2021.txt') as json_file:
+with open('json2022.txt') as json_file:
     data = json.load(json_file)
     items = data['items']
     for item in items:
         # print item["fraction"]["name"]["nl"]
-        f.write("%s,%s,%s,True,%s\n" % (item["fraction"]["name"]["nl"], item["timestamp"][0:10], item["timestamp"][0:10], item["fraction"]["logo"]["name"]["nl"]))
+        f.write("\"%s\",\"%s\",\"%s\",True,\"%s\"\n" % (item["fraction"]["name"]["nl"], item["timestamp"][0:10], item["timestamp"][0:10], item["fraction"]["logo"]["name"]["nl"]))
 
 
 f.close()
